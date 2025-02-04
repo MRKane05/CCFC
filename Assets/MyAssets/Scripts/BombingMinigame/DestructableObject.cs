2d18cd890f7638ffae98a5cfbc000386563ea190
Mark Kane <markane05@hotmail.com>
2025-02-04 17:23:56 +1300

    Strafing minigame has moving vehicles added with a destroy state

--- Assets/MyAssets/Scripts/BombingMinigame/DestructableObject.cs
+++ Assets/MyAssets/Scripts/BombingMinigame/DestructableObject.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This should maybe be in a different place as it could apply to all parts of the game, but the bomber minigame is where it was made first
-//Should this be an actor?
+//I think that having this as an actor is over-complicating things, but we could make Actor into a class that extends off of this
 public class DestructableObject : MonoBehaviour {
 
     public float Health = 500f; //I've no idea what this'll be
+    public bool bDestroyed = false;
+    public GameObject liveObject, destroyedObject;
 
-    public void TakeDamage(float damageAmount)
+
+    void Start()
+    {
+        if (!liveObject || !destroyedObject)
+        {
+            Debug.LogError("GameObject: " + gameObject.name + " does not have live/destroyed meshes set");
+        }
+        //Set our necessary states
+        liveObject.SetActive(true);
+        destroyedObject.SetActive(false);
+    }
+
+    public virtual void TakeDamage(float damageAmount)
     {
         Health -= damageAmount;
-        if (Health <=0)
+        if (Health <=0 && !bDestroyed)
         {
-            Destroy(gameObject); //remove this from the scene
+            if (liveObject != null)
+            {
+                liveObject.SetActive(false);
+            }
+            if (destroyedObject != null)
+            {
+                destroyedObject.SetActive(true);
+            }
+            bDestroyed = true;
+            //Destroy(gameObject); //remove this from the scene
             //PROBLEM: Need to add our scoring for this being a target or not to the levelController (or whatever is in charge of everything)
             ((LevelControllerBomberMinigame)LevelControllerBase.Instance).ObjectDestroyed(gameObject, this);
         }
---
+++ Assets/MyAssets/StraffingRunAssets/Scripts/ChainMovingDestructable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The idea is that this is a destructable object that moves and stops when the object in front of it becomes stopped
+public class ChainMovingDestructable : DestructableObject
+{
+    public float ForwardSpeed = 20f;    //How fast will we move forward?
+    public float SlowDownSpeed = 1f;    //How quickly will our vehicle stop
+    
+    public ChainMovingDestructable VehicleInfront;  //We could raytrace this but I don't think it's necessary
+    //I want these vehicles to move to the side (look derailed for the trains perhaps) but for the moment lets just roll with this
+
+
+    void Update()
+    {
+        if (!bDestroyed)
+        {
+            if (!VehicleInfront)
+            {
+                DoMoveForward();
+            }
+            else
+            {
+                if (!VehicleInfront.bDestroyed)
+                {
+                    DoMoveForward();
+                } else
+                {
+                    ForwardSpeed = Mathf.Lerp(ForwardSpeed, 0, Time.deltaTime * SlowDownSpeed);
+                }
+            }
+        }
+    }
+
+    void DoMoveForward()
+    {
+        gameObject.transform.position += gameObject.transform.forward * ForwardSpeed * Time.deltaTime;
+    }
+}
