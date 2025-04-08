cee177830b38b3f80ecc4b2024cd6f6b74f1e8b0
Mark Kane <markane05@hotmail.com>
2025-04-08 16:57:29 +1200

    Ack ack now does damage

--- Assets/MyAssets/Scripts/Projectiles/AckAckExplosion.cs
+++ Assets/MyAssets/Scripts/Projectiles/AckAckExplosion.cs
@@ -6,18 +6,27 @@ public class AckAckExplosion : MonoBehaviour {
 
 	public ParticleSystem[] ChildParticleSystems;
 
+	public int Team = 0;	//What is our team? This is used to control difficulty
+
 	public bool bIsActive = false;
 	public Vector3 targetLocation = Vector3.zero;
 	//Move speed will be constant for readability
-	float moveSpeed = 40f;
+	public float EffectDuration = 4f;
+	public float BlastRadius = 4f;
+	public float BlastDamage = 7f;
+	public float SparkleDamage = 1f;
+	public float SparkleFrequency = 0.5f;
+	float SparkleStart = 0;
+
 
 	// Use this for initialization
 	void Start () {
 		ChildParticleSystems = gameObject.GetComponentsInChildren<ParticleSystem>();
 	}
 	
-	public void SetupAckAck(Vector3 targetPoint, float triggerTime)
+	public void SetupAckAck(Vector3 targetPoint, float triggerTime, int newTeam)
     {
+		Team = newTeam;
 		gameObject.transform.position = targetPoint - new Vector3(Random.Range(-2f, 2f), Random.Range(5f, 8f), Random.Range(-2f, 2f));	//Set our start location as below and to the side of where we'll explode
 		targetLocation = targetPoint;
 		StartCoroutine(DoAckAckBehavior(triggerTime));
@@ -25,6 +34,7 @@ public class AckAckExplosion : MonoBehaviour {
 
 	IEnumerator DoAckAckBehavior(float detonateDelay)
     {
+		bIsActive = true;
 		float detonateTime = Time.time + detonateDelay;
 		Vector3 startLocation = gameObject.transform.position;
 		while (Time.time < detonateTime)
@@ -33,13 +43,60 @@ public class AckAckExplosion : MonoBehaviour {
 			gameObject.transform.position = Vector3.Lerp(targetLocation, startLocation, (detonateTime - Time.time) / detonateDelay);    //I guess?
 			yield return null;
         }
-
+		//Do our explosion
+		AreaDamage(gameObject.transform.position, BlastDamage);
 		foreach (ParticleSystem thisEmitter in ChildParticleSystems)
         {
 			thisEmitter.Play();
         }
 		AudioSource audio = gameObject.GetComponentInChildren<AudioSource>();
 		audio.Play();
-		Destroy(gameObject, 3.5f);	//Remove our effect
+
+		//Handle the damage from our sparkles
+		float sparkleTime = Time.time + EffectDuration;
+		while (Time.time < sparkleTime)
+        {
+			yield return new WaitForSeconds(SparkleFrequency);
+			AreaDamage(gameObject.transform.position, SparkleDamage);
+        }
+
+		bIsActive = false;
+		gameObject.SetActive(false);
     }
+
+	//I don't have this anywhere already?
+	public void AreaDamage(Vector3 position, float damage)
+    {
+		//While this is a good approach it's probably faster to go through and check our actors individually as we always know there'll be a limited number of them
+		//And it'll provide us with more information
+		/*
+		Collider[] hitColliders = Physics.OverlapSphere(position, BlastRadius);
+		foreach (Collider hitCollider in hitColliders)
+		{
+			Actor hitActor = hitCollider.gameObject.GetComponent<Actor>();
+			if (hitActor)
+            {
+				hitActor.takeDamage(damage, "FLAK", gameObject, Team, 0f);
+            }
+			//hitCollider.SendMessage("AddDamage");
+			Debug.Log("Flak Hit: " + gameObject.name);
+		}
+		*/
+		float distanceSqr = BlastRadius * BlastRadius;
+		foreach (actorWrapper thisActor in ((LevelController)LevelControllerBase.Instance).friendlyList)
+        {
+			if (Vector3.SqrMagnitude(gameObject.transform.position - thisActor.vehicle.transform.position) < distanceSqr)
+            {
+				thisActor.actor.takeDamage(damage, "FLAK", gameObject, Team, 0f);
+			}
+        }
+
+		foreach (actorWrapper thisActor in ((LevelController)LevelControllerBase.Instance).enemyList)
+		{
+			if (Vector3.SqrMagnitude(gameObject.transform.position - thisActor.vehicle.transform.position) < distanceSqr)
+			{
+				thisActor.actor.takeDamage(damage, "FLAK", gameObject, Team, 0f);
+			}
+		}
+	}
 }
--- Assets/MyAssets/Scripts/VehicleControllers/zone_AckAck.cs
+++ Assets/MyAssets/Scripts/VehicleControllers/zone_AckAck.cs
@@ -6,10 +6,13 @@ using System.Collections.Generic;
 public class zone_AckAck : MonoBehaviour {
 
 	float lastTriggerTime =0;
-	float reTriggerTime = 0.5f;
+	float reTriggerTime = 0.5f;	//This will target our player
+	float volumeTriggerTime = 1f;	//This will go "around" our player for effect
 
 	public GameObject ackAckPrefab;
-	public float ZoneWidth = 20f;
+	public float ZoneWidth = 50f;	//How big is the zone of the Ack Ack (this will be set on setup)
+	public float EffectZoneSize = 30f; //How bid of a zone do we have for Ack Ack effects firing in front of the player?
+	public float MinimumHeight = 10f;
 	public Range LeadRange = new Range(2f, 7f);
 
 	public int Team = 0;
@@ -46,13 +49,15 @@ public class zone_AckAck : MonoBehaviour {
 	// have the Ack Ack seek the enemy fighters to some degree...which now means that this
 	//thing needs a team and class extension etc.
 	//It needs to be EXCITING!
-	void Update () {
+	void Update()
+	{
 		//We need to check if something we want to shoot at is in this zone
 		//We need functions to handle the size of the zone
 		//We might need something to look at targets and "cluster" according to what we're shooting at
 		//Could gameify the flak even further by having a small "brew point" before it explodes, giving the player the opportunity to dodge
 
-		if (Time.time - reTriggerTime > lastTriggerTime) {
+		if (Time.time - reTriggerTime > lastTriggerTime)
+		{
 			//make an Ack Ack!
 			lastTriggerTime = Time.time;
 
@@ -66,14 +71,56 @@ public class zone_AckAck : MonoBehaviour {
 				Vector3 targetPosition = ZoneActors[TargetActor].vehicle.gameObject.transform.position + ZoneActors[TargetActor].vehicle.gameObject.transform.forward * LeadRange.GetRandom();
 				//We could do with a random on the Ack Ack
 				targetPosition += new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-				GameObject newAckAck = Instantiate(ackAckPrefab, targetPosition, Quaternion.identity);
+				GameObject newAckAck = GetAckAckEffect(); //Instantiate(ackAckPrefab, targetPosition, Quaternion.identity);
+				newAckAck.transform.position = targetPosition;
+				newAckAck.SetActive(true); //Turn this effect on
 				AckAckExplosion newAckAckScript = newAckAck.GetComponent<AckAckExplosion>();
-				newAckAckScript.SetupAckAck(targetPosition, Random.Range(0.5f, 1.2f));
+				newAckAckScript.SetupAckAck(targetPosition, Random.Range(0.5f, 1.2f), Team);
 
 			}
-
-			//We should also put down "background" ack ack for effect
-			//Instantiate(ackAckPrefab, spawnPoint, Quaternion.identity);
 		}
+
+		if (Time.time - reTriggerTime > volumeTriggerTime && PlayerController.Instance != null)
+		{
+			//Make a "background" Ack Ack
+			volumeTriggerTime = Time.time;
+			//This is only worthwhile if our player sees it, so lets see about making a pattern that'll put it within a forward zone of the player
+			//Of course the clever player could use this as an offensive weapon, but Ack Ack is known for that
+			Vector3 targetPosition = PlayerController.Instance.transform.position + PlayerController.Instance.transform.forward * EffectZoneSize;
+			targetPosition += new Vector3(Random.Range(-EffectZoneSize, EffectZoneSize), Random.Range(-EffectZoneSize, EffectZoneSize), Random.Range(-EffectZoneSize, EffectZoneSize));
+			//We need something to check and see if this point is under the terrain
+			if (targetPosition.z > MinimumHeight) //We can do a bang here
+            {
+				GameObject newAckAck = GetAckAckEffect(); //Instantiate(ackAckPrefab, targetPosition, Quaternion.identity);
+				newAckAck.transform.position = targetPosition;
+				newAckAck.SetActive(true); //Turn this effect on
+				AckAckExplosion newAckAckScript = newAckAck.GetComponent<AckAckExplosion>();
+				newAckAckScript.SetupAckAck(targetPosition, Random.Range(0.5f, 1.2f), Team);
+			}
+		}
+	}
+
+	GameObject GetAckAckEffect()
+    {
+		bool bHasFoundEffect = false;
+		GameObject newAckAck = null;
+		foreach (GameObject thisEffect in EffectPrefabs)
+        {
+			if (!bHasFoundEffect)
+			{
+				if (!thisEffect.GetComponent<AckAckExplosion>().bIsActive)
+				{
+					bHasFoundEffect = true;
+					newAckAck = thisEffect;
+				}
+			}
+        }
+		if (!bHasFoundEffect)
+		{
+			newAckAck = Instantiate(ackAckPrefab) as GameObject;
+			EffectPrefabs.Add(newAckAck);
+		}
+
+		return newAckAck;
 	}
 }
