e4f4d4925c00dd2689d8f7d9f5bc46b9f433dffa
Mark Kane <markane05@hotmail.com>
2026-01-21 17:28:52 +1300

    Started moving the system into one with dynamically setup aircraft with the new system

---
+++ Assets/MyAssets/Scripts/VehicleControllers/AircraftModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//A base class for setting up the apparence of one of our models. This handles things like colour and maybe later textures
+public class AircraftModel : MonoBehaviour {
+	public GameObject modelBase;	//This'll be a LOD group
+	public GameObject outlineBase;  //And this'll be a LOD group
+
+	public Material modelMat;
+	public Material outlineMat;
+	
+	void Start () {
+		//Sort out our materials
+
+		LODGroup lodGroup = modelBase.GetComponent<LODGroup>();
+		if (lodGroup)
+        {
+			modelMat = new Material(modelBase.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial);
+			foreach (Transform child in modelBase.transform)
+            {
+				child.GetComponent<Renderer>().material = modelMat;
+            }
+        } else
+        {
+			modelMat = new Material(modelBase.GetComponent<Renderer>().sharedMaterial);
+			modelBase.GetComponent<Renderer>().material = modelMat;
+		}
+		//And our outline (which will be used for targeting)
+		LODGroup outlineLodGroup = modelBase.GetComponent<LODGroup>();
+		if (outlineLodGroup)
+		{
+			outlineMat = new Material(outlineBase.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial);
+			foreach (Transform child in outlineBase.transform)
+			{
+				child.GetComponent<Renderer>().material = outlineMat;
+			}
+		}
+		else
+		{
+			outlineMat = new Material(outlineBase.GetComponent<Renderer>().sharedMaterial);
+			outlineBase.GetComponent<Renderer>().material = modelMat;
+		}
+	}
+}
---
+++ Assets/MyAssets/Shaders/VertexLitPipeline/CCFC-OutlineOnly.shader
@@ -0,0 +1,73 @@
+﻿Shader "CCFC/VertexLit/CCFC-OutlineOnly"
+{
+	Properties
+	{
+		_Color("Outline Color", Color) = (0,0,0,1)
+		_Outline("Outline width", Range(.002, 0.1)) = .005
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+		Cull Front
+		ZWrite On
+		ColorMask RGB
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float3 normal : NORMAL;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed4 _Color;
+			float _Outline;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				float3 norm = mul((float3x3)UNITY_MATRIX_IT_MV, v.normal);
+				float2 offset = TransformViewToProjection(norm.xy);
+
+				o.vertex.xy += offset * o.vertex.z * _Outline;
+				//o.vertex.xyz += norm.xyz * _Outline;
+
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag(v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = _Color; // tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
